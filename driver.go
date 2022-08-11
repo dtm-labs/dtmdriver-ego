8089b556a89a13e779faef9a581176d1c6a82de5
wwg <wuwg@vector-china.cn>
2022-08-11 13:28:40 +0800

    first commit

--- driver.go
+++ driver.go
@@ -1,15 +1,26 @@
 package driver
 
 import (
+	"bytes"
 	"context"
+	"errors"
 	"fmt"
 	"net/url"
+	"strconv"
 	"strings"
+	"time"
 
+	"github.com/BurntSushi/toml"
 	"github.com/dtm-labs/dtmdriver"
+	"github.com/gotomicro/ego-component/eetcd"
+	"github.com/gotomicro/ego-component/eetcd/registry"
+	"github.com/gotomicro/ego-component/ek8s"
+	k8sregistry "github.com/gotomicro/ego-component/ek8s/registry"
+	"github.com/gotomicro/ego/client/egrpc/resolver"
 	"github.com/gotomicro/ego/core/constant"
+	"github.com/gotomicro/ego/core/econf"
+	"github.com/gotomicro/ego/core/eregistry"
 	"github.com/gotomicro/ego/server"
-	"github.com/vicnoah/dtmdriver-ego/plugin/egodriver"
 )
 
 const (
@@ -22,15 +33,19 @@ type (
 	egoDriver struct{}
 )
 
+var registerMap = make(map[string]eregistry.Registry, 0)
+
 func (e *egoDriver) GetName() string {
 	return DriverName
 }
 
-func (e *egoDriver) RegisterGrpcResolver() {
-	egodriver.Register()
+func (e *egoDriver) RegisterAddrResolver() {
+	for name, reg := range registerMap {
+		resolver.Register(name, reg)
+	}
 }
 
-func (e *egoDriver) RegisterGrpcService(target string, endpoint string) error {
+func (e *egoDriver) RegisterService(target string, endpoint string) error {
 	if target == "" { // empty target, no action
 		return nil
 	}
@@ -40,6 +55,8 @@ func (e *egoDriver) RegisterGrpcService(target string, endpoint string) error {
 		return err
 	}
 
+	query, _ := url.ParseQuery(u.RawQuery)
+
 	info := server.ApplyOptions(
 		server.WithScheme("grpc"),
 		server.WithAddress(endpoint),
@@ -47,7 +64,88 @@ func (e *egoDriver) RegisterGrpcService(target string, endpoint string) error {
 		server.WithName(u.Host),
 	)
 
-	return egodriver.RegisterMap[u.Scheme].RegisterService(context.Background(), &info)
+	var reg eregistry.Registry
+
+	switch u.Scheme {
+	case KindEtcd:
+		conf := eetcd.DefaultConfig()
+		conf.Addrs = strings.Split(u.Host, ",")
+		if query.Get("certFile") != "" {
+			conf.CertFile = query.Get("certFile")
+		}
+		if query.Get("keyFile") != "" {
+			conf.KeyFile = query.Get("keyFile")
+		}
+		if query.Get("caCert") != "" {
+			conf.CaCert = query.Get("caCert")
+		}
+		if query.Get("userName") != "" {
+			conf.UserName = query.Get("userName")
+		}
+		if query.Get("connectTimeout") != "" {
+			du, _ := time.ParseDuration(query.Get("connectTimeout"))
+			conf.ConnectTimeout = du
+		}
+		if query.Get("autoSyncInterval") != "" {
+			du, _ := time.ParseDuration(query.Get("autoSyncInterval"))
+			conf.AutoSyncInterval = du
+		}
+		if query.Get("enableBasicAuth") != "" {
+			b, _ := strconv.ParseBool(query.Get("enableBasicAuth"))
+			conf.EnableBasicAuth = b
+		}
+		if query.Get("enableSecure") != "" {
+			b, _ := strconv.ParseBool(query.Get("enableSecure"))
+			conf.EnableSecure = b
+		}
+		if query.Get("enableBlock") != "" {
+			b, _ := strconv.ParseBool(query.Get("enableBlock"))
+			conf.EnableBlock = b
+		}
+		if query.Get("enableFailOnNonTempDialError") != "" {
+			b, _ := strconv.ParseBool(query.Get("enableFailOnNonTempDialError"))
+			conf.EnableFailOnNonTempDialError = b
+		}
+		var buf = new(bytes.Buffer)
+		buf.WriteString(fmt.Sprintf("[%s]\n", u.Scheme))
+		toml.NewEncoder(buf).Encode(&conf)
+		buf.WriteString(fmt.Sprintf("\n[registry]\nscheme = \"%s\"", u.Scheme))
+		econf.LoadFromReader(buf, toml.Unmarshal)
+
+		reg = registry.Load("registry").Build(registry.WithClientEtcd(eetcd.Load("etcd").Build()))
+	case KindK8S:
+		conf := ek8s.DefaultConfig()
+		conf.Addr = u.Host
+		if query.Get("debug") != "" {
+			b, _ := strconv.ParseBool(query.Get("debug"))
+			conf.Debug = b
+		}
+		if query.Get("token") != "" {
+			conf.Token = query.Get("token")
+		}
+		if query.Get("namespaces") != "" {
+			conf.Namespaces = strings.Split(query.Get("namespaces"), ",")
+		}
+		if query.Get("deploymentPrefix") != "" {
+			conf.DeploymentPrefix = query.Get("deploymentPrefix")
+		}
+		if query.Get("tlsClientConfigInsecure") != "" {
+			b, _ := strconv.ParseBool(query.Get("tlsClientConfigInsecure"))
+			conf.TLSClientConfigInsecure = b
+		}
+		var buf = new(bytes.Buffer)
+		buf.WriteString(fmt.Sprintf("[%s]\n", u.Scheme))
+		toml.NewEncoder(buf).Encode(&conf)
+		buf.WriteString(fmt.Sprintf("\n[registry]\nscheme = \"%s\"", u.Scheme))
+		econf.LoadFromReader(buf, toml.Unmarshal)
+
+		reg = k8sregistry.Load("registry").Build(k8sregistry.WithClient(ek8s.Load(u.Scheme).Build()))
+	}
+
+	if reg != nil {
+		return errors.New("register error")
+	}
+	return reg.RegisterService(context.Background(), &info)
 }
 
 // etcd:///<服务名称>/api.hello/TransOut
--- go.mod
+++ go.mod
@@ -4,16 +4,25 @@ go 1.15
 
 require (
 	github.com/BurntSushi/toml v1.1.0
-	github.com/dtm-labs/dtmdriver v0.0.3
-	github.com/dtm-labs/dtmdriver-clients v0.0.0-20220417005714-16cff989c11f
-	github.com/dtm-labs/dtmgrpc v1.14.2
-	github.com/gotomicro/ego v1.1.3
+	github.com/dtm-labs/dtmdriver v0.0.6
+	github.com/google/gofuzz v1.2.0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/gotomicro/ego v1.1.4
 	github.com/gotomicro/ego-component/eetcd v0.2.3
-	github.com/kr/pretty v0.3.0 // indirect
+	github.com/gotomicro/ego-component/ek8s v0.2.3
 	github.com/matttproud/golang_protobuf_extensions v1.0.2-0.20181231171920-c182affec369 // indirect
+	github.com/prometheus/client_golang v1.12.2 // indirect
 	github.com/rogpeppe/go-internal v1.8.1 // indirect
-	github.com/stretchr/testify v1.7.2 // indirect
-	golang.org/x/sys v0.0.0-20220503163025-988cb79eb6c6 // indirect
-	google.golang.org/grpc v1.46.2 // indirect
-	google.golang.org/protobuf v1.28.0
+	github.com/stretchr/testify v1.8.0 // indirect
+	go.etcd.io/etcd/client/v3 v3.5.4 // indirect
+	go.opentelemetry.io/otel v1.8.0 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.8.0 // indirect
+	golang.org/x/net v0.0.0-20220531201128-c960675eff93 // indirect
+	golang.org/x/time v0.0.0-20220411224347-583f2d630306 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20220602131408-e326c6e8e9c8 // indirect
+	k8s.io/client-go v0.22.9 // indirect
+	k8s.io/klog/v2 v2.40.1 // indirect
+	k8s.io/utils v0.0.0-20220706174534-f6158b442e7c // indirect
 )
